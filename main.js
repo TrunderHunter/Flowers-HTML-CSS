2fbaf8b50b9d95e0b47a855f5779126bd9b12cca
TranVanHung_20061461 <hungtran2002a2@gmail.com>
2026-03-08 11:20:17 +0700

    Add butterfly animations and reveal logic to enhance visual effects

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 onload = () => {
+  const FLOWER_REVEAL_DELAY_MS = 6000;
+  const butterflies = document.querySelector(".butterflies");
+
   document.body.classList.remove("container");
+
+  if (butterflies) {
+    setTimeout(() => {
+      butterflies.classList.add("butterflies--active");
+    }, FLOWER_REVEAL_DELAY_MS);
+  }
 };
